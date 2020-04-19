e5af2db2d2e9688f883369f9a5ec92ab4c36b0d0
Halley E. Froehlich <hefroehlich@128-111-64-241.vpn.ucsb.edu>
2020-04-19 09:22:55 -0700

    ggplot covid cases plot

--- code/covid_tsmap.R
+++ code/covid_tsmap.R
@@ -10,7 +10,13 @@ library(rgdal)
 library(stats)
 library(ggmap)
 library(rworldmap)
+library(dplyr)
+library(tidyr)
+library(janitor)
+library(lubridate)
 
+
+setwd("~/Desktop/github/covid19/data")
 
 #colors
 cols = rev(colorRampPalette(brewer.pal(11, 'Spectral'))(255)) # rainbow color scheme for plotting
@@ -18,8 +24,35 @@ cols = rev(colorRampPalette(brewer.pal(11, 'Spectral'))(255)) # rainbow color sc
 #Our World in Data COVID (pulled both per million and totals)
 covid_permil<-read.csv("covid-confirmed-cases-per-million-since-1-per-million.csv") 
 head(covid_permil)
-covid_totl<-read.csv("total-cases-covid-19.csv") 
+covid_tot<-read.csv("total-cases-covid-19.csv") 
 head(covid_tot)
+
+#DEAL WITH DATE
+covid_permil$Date <- mdy(covid_permil$Date)
+head(covid_permil)
+
+#CLEAN NAMES
+cl_covid <- covid_permil %>%
+  clean_names()
+head(cl_covid)
+
+#PICK STATES
+id<- c("USA","CHN","IND")
+cnty_covid<-filter(cl_covid, iso3 %in% id)
+head(cnty_covid)
+
+#PLOT WITH EXAMPLE DATES (MODIFY BASED ON NEWS SELECTION)
+ggplot(cnty_covid, aes(x=date, y=log(x_cases_per_million+1), group=entity)) +
+  geom_line(aes(color=entity), size=2)+
+  labs(x = "Time", y="LN(Confirmed Cases per Million")+
+  scale_x_date(date_labels = "%Y %b %d")+
+  geom_vline(xintercept = as.numeric(as.Date(c("2020-01-23", "2020-03-01"))), linetype=2, 
+             color=c("black", "gray"))+
+  theme_classic()+
+  theme(legend.title=element_blank())+
+  scale_color_brewer(palette="PuBu")
+
+p
 
 #Our World in Data seafood per captia
 sea_data<-read.csv("fish-and-seafood-consumption-per-capita.csv") 
