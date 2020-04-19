16e9296efe69e7d9eb4aee4a09c33e694b2f9d1f
Halley E. Froehlich <hefroehlich@128-111-64-241.vpn.ucsb.edu>
2020-04-19 10:00:58 -0700

    Basic map code. Need to link to seafood per capita data.

--- code/covid_tsmap.R
+++ code/covid_tsmap.R
@@ -14,7 +14,13 @@ library(dplyr)
 library(tidyr)
 library(janitor)
 library(lubridate)
+library(patchwork)
 
+install.packages(c("cowplot", "googleway", "ggplot2", "ggrepel", "ggspatial", "libwgeom", "sf", "rnaturalearth", "rnaturalearthdata"))
+library("rnaturalearth")
+library("rnaturalearthdata")
+theme_set(theme_bw())
+library("sf")
 
 setwd("~/Desktop/github/covid19/data")
 
@@ -40,9 +46,10 @@ head(cl_covid)
 id<- c("USA","CHN","IND")
 cnty_covid<-filter(cl_covid, iso3 %in% id)
 head(cnty_covid)
+max_pt<-log(max(cnty_covid$x_cases_per_million+1))
 
 #PLOT WITH EXAMPLE DATES (MODIFY BASED ON NEWS SELECTION)
-ggplot(cnty_covid, aes(x=date, y=log(x_cases_per_million+1), group=entity)) +
+p1<-ggplot(cnty_covid, aes(x=date, y=log(x_cases_per_million+1), group=entity)) +
   geom_line(aes(color=entity), size=2)+
   labs(x = "Time", y="LN(Confirmed Cases per Million")+
   scale_x_date(date_labels = "%Y %b %d")+
@@ -50,19 +57,28 @@ ggplot(cnty_covid, aes(x=date, y=log(x_cases_per_million+1), group=entity)) +
              color=c("black", "gray"))+
   theme_classic()+
   theme(legend.title=element_blank())+
-  scale_color_brewer(palette="PuBu")
+  scale_color_brewer(palette="PuBu")+
+  geom_label(label=c("A."), x=as.numeric(as.Date(c("2020-01-23"))), y=c(max_pt))+
+  geom_label(label=c("B."), x=as.numeric(as.Date(c("2020-03-01"))), y=c(max_pt))
 
-p
 
 #Our World in Data seafood per captia
 sea_data<-read.csv("fish-and-seafood-consumption-per-capita.csv") 
 head(sea_data)
 
-sPDF.imp <- joinCountryData2Map(sea_data ,joinCode = "ISO3", nameJoinColumn = "ISO3", verbose=TRUE)
-catmethod=seq(1, 6, by=1) 
-numCats <- 6
-colourPalette <- colorRampPalette(brewer.pal(11, "Spectral")) (6)
+sea_2013<-filter(sea_data, Year==2013)
+head(sea_data)
+names(sea_data) <- c("region","iso3","year","per_cap_kg")
+head(sea_data)
 
-#quartz()
-mapCountryData(sPDF.imp,nameColumnToPlot='Check', colourPalette=colourPalette, catMethod=catmethod,
-               missingCountryCol="gray",addLegend=F,oceanCol="white", mapTitle="")
+world_map <- map_data("world",sea_data)
+head(world_map)
+
+comb_data<- left_join(world_map,sea_data, by=region)
+
+p2<-ggplot(world_map, aes(x = long, y = lat, group = group)) +
+  geom_polygon(fill="lightgray", colour = "white")
+p2
+
+#PLOT TIME-LINE AND MAP TOGETHER
+p1/p2
