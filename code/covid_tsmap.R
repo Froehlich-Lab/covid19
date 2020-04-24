dfbc72dc2ae159e6e020df55020024f57de4b5dd
Halley E. Froehlich <hefroehlich@halleys-mbp.lan>
2020-04-24 09:19:53 -0700

    added Myanmar to covid plot and removed non-impact new events.

--- code/covid_tsmap.R
+++ code/covid_tsmap.R
@@ -1,3 +1,4 @@
+
 rm(list=ls())
 
 library(chron)
@@ -24,19 +25,16 @@ library("rnaturalearthdata")
 theme_set(theme_bw())
 library("sf")
 
-#setwd("~/Desktop/github/covid19/data")
-
-#colors
-cols = rev(colorRampPalette(brewer.pal(11, 'Spectral'))(255)) # rainbow color scheme for plotting
+setwd("~/Desktop/github/covid19/data")
 
 #Our World in Data COVID (pulled both per million and totals)
-covid_permil<-read.csv("covid-confirmed-cases-per-million-since-1-per-million.csv") 
+covid_permil<-read_csv("covid-confirmed-cases-per-million-since-1-per-million.csv") 
 covid_permil <- read_csv(here("data", "covid-confirmed-cases-per-million-since-1-per-million.csv"))
 
 head(covid_permil)
-covid_tot<-read.csv("total-cases-covid-19.csv") 
+covid_tot<-read_csv("total-cases-covid-19.csv") 
 head(covid_tot)
-chn_news<-read.csv("china_news.csv") 
+chn_news<-read_csv("china_news.csv") 
 head(chn_news)
 
 #NEWS BACK BONE OF DATES AND COUNTRIES OF INTEREST
@@ -44,7 +42,7 @@ chn_news$clean_date <- mdy(chn_news$clean_date)
 head(chn_news)
 
   dates <- na.omit(unique(chn_news$clean_date))
-  dates #10 total dates of interest dates[c(1,4,5,7:10)]
+  dates #10 total dates picking the EARLIEST DATE to plot for a given time period
   link<- na.omit(unique(chn_news$linked_countires))
   link #10 countries
 
@@ -60,25 +58,25 @@ unique(cl_covid$iso3)
 
 #PICK STATES
 id<- c("USA","CHN","IND","AUS", "CHL", "NOR",
-       "PER","CAN", "ZAF", "KEN")
+       "PER","CAN", "ZAF", "KEN", "MMR")
 cnty_covid<-filter(cl_covid, iso3 %in% id)
 head(cnty_covid)
-max_pt<-log(max(cnty_covid$x_cases_per_million+1))
+max_pt<-log(max(cnty_covid$cases_per_million+1))
 
 #PLOT WITH EXAMPLE DATES (MODIFY BASED ON NEWS SELECTION)
 max_day<-max(as.Date(cnty_covid$date))
 quartz()
-p1<-ggplot(cnty_covid, aes(x=date, y=log(x_cases_per_million+1), group=entity)) +
+p1<-ggplot(cnty_covid, aes(x=date, y=log(cases_per_million+1), group=entity)) +
   geom_line(aes(color=entity), size=2)+
   labs(x = "Time", y="LN(Confirmed Cases per Million)")+
   scale_x_date(date_labels = "%Y %b %d")+
   #geom_vline(xintercept = as.numeric(as.Date(c("2020-01-23", "2020-03-01"))), linetype=2,color="black", "gray")+
-  geom_vline(xintercept = as.numeric(as.Date(dates[c(1,4,5,7:10)])), linetype=2,color="black")+  
+  geom_vline(xintercept = as.numeric(as.Date(dates[c(1,4,5,8,10,12)])), linetype=2,color="black")+  
   theme_classic()+
   scale_color_viridis(discrete = TRUE, option = "D")+
-  geom_text(data = filter(cnty_covid, date == max_day), aes(label = entity, colour = entity, x = max_day, y = log(x_cases_per_million+1)), hjust = -.1)+
+  geom_text(data = filter(cnty_covid, date == max_day), aes(label = entity, colour = entity, x = max_day, y = log(cases_per_million+1)), hjust = -.1)+
   theme(legend.position = 'none', plot.margin = unit(c(1,4,1,1), "lines"))+
-  annotate("text", x = as.Date(dates[c(1,4,5,7:10)]-2), y=c(max_pt), label = c("A.","B.","C.","D.","E.","F.","G."))
+  annotate("text", x = as.Date(dates[c(1,4,5,8,10,12)]-2), y=c(max_pt), label = c("A.","B.","C.","D.","E.","F."))
  
 quartz()
 gt <- ggplotGrob(p1)
