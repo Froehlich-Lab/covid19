d354e0e62b68a756aaf90d230b50349578ddc600
Richard Cottrell <cottrell@aurora.nceas.ucsb.edu>
2020-05-25 23:39:01 -0700

    Trying to put labels on map. Damn centroids

--- code/covid_tsmap.R
+++ code/covid_tsmap.R
@@ -41,11 +41,13 @@ head(covid_tot)
 chn_news<-read_csv(here("data", "china_news.csv"))
 head(chn_news)
 
+
 #NEWS BACK BONE OF DATES AND COUNTRIES OF INTEREST
 chn_news$clean_date <- mdy(chn_news$clean_date)
 head(chn_news)
 
-  dates <- na.omit(unique(chn_news$clean_date))
+  dates <- c(na.omit(unique(chn_news$clean_date)), ymd("2020-03-11"))
+  
   dates #10 total dates picking the EARLIEST DATE to plot for a given time period
   link<- na.omit(unique(chn_news$linked_countires))
   link #10 countries
@@ -65,24 +67,25 @@ id<- c("USA","CHN","IND","AUS", "CHL", "NOR",
        "PER","ZAF", "KEN", "MMR")
 cnty_covid<-filter(cl_covid, iso3 %in% id)
 head(cnty_covid)
-max_pt<-log(max(cnty_covid$cases_per_million+1))
+max_pt<-log10(max(cnty_covid$cases_per_million+1))
 
 #PLOT WITH EXAMPLE DATES (MODIFY BASED ON NEWS SELECTION)
 max_day<-max(as.Date(cnty_covid$date))
 
-p1<-ggplot(cnty_covid, aes(x=date, y=log(cases_per_million+1), group=entity)) +
+p1<-ggplot(cnty_covid, aes(x=date, y=log10(cases_per_million+1), group=entity)) +
   geom_line(aes(color=entity), size=2)+
-  labs(x = "Time", y="LN(Confirmed Cases per Million)")+
+  labs(x = "Time", y=bquote(log[10]~Cases~per~million))+
   scale_x_date(date_labels = "%Y %b %d")+
   #geom_vline(xintercept = as.numeric(as.Date(c("2020-01-23", "2020-03-01"))), linetype=2,color="black", "gray")+
-  geom_vline(xintercept = as.numeric(as.Date(dates[c(1,4,5,8,10,12)])), linetype=2,color="black")+  
+  geom_vline(xintercept = as.numeric(dates[c(1,4,5,8,10,12)]), linetype=2, color="black")+
+  annotate("segment", x=dates[13], xend=dates[13], y=-Inf, yend=Inf, colour="red", linetype=2)+
   theme_classic()+
   scale_color_viridis(discrete = TRUE, option = "D")+
   geom_text(data = filter(cnty_covid, date == max_day), aes(label = entity,
                                                             colour = entity,
                                                             x = max_day, 
-                                                            y = case_when(entity %in% c("Peru", "South Africa") ~ log(cases_per_million+1)-0.2,
-                                                                          TRUE ~ log(cases_per_million+1))),
+                                                            y = case_when(entity %in% c("Peru", "South Africa") ~ log10(cases_per_million+1)-0.2,
+                                                                          TRUE ~ log10(cases_per_million+1))),
             size=3,
             hjust = -.1)+
   theme(legend.position = 'none', plot.margin = unit(c(1,4,1,1), "lines"))+
